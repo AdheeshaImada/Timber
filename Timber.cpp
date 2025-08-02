02e2288fa9d1b9323333c439380c8458d364ab10
AdheeshaImada <adheeshaimadakulasekara@gmail.com>
2025-08-02 17:40:40 +0530

    Open the window one time and then close. work all fine

--- Timber.cpp
+++ Timber.cpp
@@ -1,5 +1,22 @@
+// Include important libraries
+
+#include <SFML/Graphics.hpp>
+
+
+// Make code easier to type with "using namespace"
+
+using namespace sf;
+
+
 //This is where game starts
 
 int main() {
+
+	// Create video mode object
+	VideoMode vm(1920, 1080);
+
+	// Create and open a window for the game
+	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
 	return 0;
 }
